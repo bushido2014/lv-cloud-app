eaa78aaadb296ca5d1f7113f655474c12cd3adf6
Viorel Soltan <joomjoom2011@gmail.com>
2025-02-27 10:38:25 +0000

    header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,6 +4,9 @@
     @include('partials.head', ['title' => 'Home'])  
 </head>
 <body>
+<header class="py-4 container mx-auto px-2 bg-blue-900 text-white">
+<a href="/">Home</a>
+</header>
     <div class="max-w-7xl mx-auto px-4">
     <h1>Home page</h1>
     </div>
