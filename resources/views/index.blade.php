5f73164ee85b7a38c51937b4e6d6cfb03b7e7f32
Viorel Soltan <joomjoom2011@gmail.com>
2025-02-27 10:45:35 +0000

    change header

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,12 @@
     @include('partials.head', ['title' => 'Home'])  
 </head>
 <body>
-<header class="py-4 container mx-auto px-2 bg-blue-900 text-white">
+<header class="py-4 px-2">
+<div class="container mx-auto bg-sky-800 text-white">
 <a href="/">Home</a>
+</div>
 </header>
-    <div class="max-w-7xl mx-auto px-4">
+    <div class="max-w-7xl mx-auto px-4 py-3">
     <h1>Home page</h1>
     </div>
 </body>
