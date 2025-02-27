41e1b90dfa95bf1412df20e9a24b89469ca9ad86
Viorel Soltan <joomjoom2011@gmail.com>
2025-02-27 07:47:34 +0000

    change i

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('partials.head', ['title' => 'Home'])  
+</head>
+<body>
+    <div class="max-w-7xl mx-auto px-4">
+    <h1>Home page</h1>
+    </div>
+</body>
+</html>
