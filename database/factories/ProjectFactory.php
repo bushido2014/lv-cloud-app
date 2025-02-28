6e5762f6aa9812ea8ea38067f8f2bce7cfb856ed
Viorel Soltan <joomjoom2011@gmail.com>
2025-02-28 12:55:09 +0000

    add factory

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Project;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Project>
+ */
+class ProjectFactory extends Factory
+{
+    protected $model = Project::class;
+
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->sentence(3),
+            'description' => $this->faker->paragraph(),
+            'image' => 'images/' . $this->faker->image('public/storage/images', 640, 480, null, false)
+        ];
+    }
+}
