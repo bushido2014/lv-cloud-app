b24668f7abc3588fc6045b01653c8e64dbd27af5
Viorel Soltan <joomjoom2011@gmail.com>
2025-03-03 13:32:30 +0000

    message remove

--- resources/views/components/message.blade.php
+++ resources/views/components/message.blade.php
@@ -1,11 +1,17 @@
 @if (session('message'))
-    <div class="w-full bg-green-400 p-3 my-3">
+    <div class="w-full bg-green-400 p-3 my-3 rounded-md" id="message">
         {{ session('message') }}
     </div>
 @endif
 
 @if (session('error'))
-    <div class="w-full bg-red-400 p-3">
+    <div class="w-full bg-red-400 p-3 my rounded-md" id="message">
         {{ session('error') }}
     </div>
 @endif
+
+<script>
+    setTimeout(() => {
+        document.getElementById('message').remove();
+    }, 3000);
+</script>
