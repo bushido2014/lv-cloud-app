739ca9fbb26c9ef90e3b109845e01a33f4911fe7
Viorel Soltan <joomjoom2011@gmail.com>
2025-03-04 13:39:37 +0000

    c

--- resources/views/livewire/projects/show-projects.blade.php
+++ resources/views/livewire/projects/show-projects.blade.php
@@ -46,9 +46,7 @@
                 wire:click="delete({{ $project->id }})">Delete</flux:button> -->
 
                 <flux:button  wire:click="edit({{ $project->id }})">Edit</flux:button>
-                <!-- <flux:button variant="danger" 
-                wire:click="$emit('openModal', 'confirmDelete', {{ $project->id }})">Delete</flux:button>
-                </div> -->
+               
                 <flux:button variant="danger"
     @dispatch('openModal', name: 'confirmDelete', projectId: {{ $project->id }})>
     Delete
@@ -62,9 +60,9 @@
         <h2 class="text-lg font-semibold">Confirm Deletion</h2>
         <p>Are you sure you want to delete this project?</p>
         <div class="mt-4 flex justify-end">
-            <flux:button @dispatch('deleteProject', projectId: {{ $project->id }}) variant="danger">
-                Delete
-            </flux:button>
+        <flux:button @dispatch('deleteProject', projectId: {{ $project->id }})" variant="danger">
+    Delete
+</flux:button>
             <flux:button modal-close class="ml-2 px-4 py-2">Cancel</flux:button>
         </div>
     </div>
